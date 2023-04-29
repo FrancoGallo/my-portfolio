8da3f5504639494e05e35487e24da158bf728529
Franco Gallo <gallofrancogabriel@gmail.com>
2023-04-29 13:11:37 +0200

    Styles in section start

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const HelloWorld = () => {
   return (
-    <h2>Hola mundo</h2>
+    <section className='hello-world-style-container'>
+      <h2>Hola mundo</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sed labore ducimus quasi dolores. Voluptatibus, obcaecati rerum! Vitae, ducimus quis placeat deserunt sequi illum a dolorum est impedit, mollitia modi?
+        Harum tenetur dolorem illum iste quis ipsa ex magnam commodi a molestiae ipsam possimus rerum aliquid, maxime autem ad minima! Laudantium nesciunt accusantium sit! Quo dolore recusandae debitis facere delectus?
+        Sequi est dignissimos accusantium nam dicta voluptatibus blanditiis esse modi rerum sed aliquid animi, sint delectus natus magni saepe sapiente. Harum at fuga deleniti, voluptatem minus ipsam quod omnis corrupti.
+      </p>
+    </section>
+    
   )
 }
 
