bd32af8088d5e4c36193a8d25b4c059d52b36cf6
Franco Gallo <gallofrancogabriel@gmail.com>
2023-08-04 19:20:25 +0200

    Checking form validations

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,43 +1,76 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+// import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
 import Button from 'react-bootstrap/Button';
+import { Formik } from 'formik';
 
 const Form = () => {
-  const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  // Send function (start) ----->
+  // const form = useRef();
 
-    emailjs.sendForm('service_a8gt7hr', 'template_oxeenlb', form.current, 'T_zupu-XKSJ-dgvid')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-  }
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   emailjs.sendForm('service_a8gt7hr', 'template_oxeenlb', form.current, 'T_zupu-XKSJ-dgvid')
+  //     .then((result) => {
+  //         console.log(result.text);
+  //     }, (error) => {
+  //         console.log(error.text);
+  //     });
+  // }
+  // Send function (end) ----->
 
   return (
-    <section className='form-style-container'>
-      <form ref={form} onSubmit={sendEmail}>
-        <div className='input-style'>
-          <label for="input" className='text'>Nombre:</label>
-          <input type="text" placeholder="Aqui tu nombre" name="user_name" className='input'/>
-        </div>
-        <div className='input-style'>
-          <label for="input" className='text'>Apellido/s:</label>
-          <input type="text" placeholder="Aqui tu apellido/s" name="user_last_name" className='input'/>
-        </div>
-        <div className='input-style'>
-          <label for="input" className='text'>Mail</label>
-          <input type="text" placeholder="Aqui tu mail" name="user_email" className='input'/>
-        </div>
-        <div className='input-style'>
-          <label for="input" className='text'>Mensaje</label>
-          <textarea name="message" placeholder="Escribe lo que quieras" className='input' />
-        </div>
-        <Button variant="outline-primary" type="submit">Enviar</Button>
-      </form>  
-    </section>
+    <Formik
+      initialValues={{
+        name: '',
+        email: ''
+      }}
+
+      onSubmit = {() => {console.log('Formulario enviado')}}
+    >
+      {({ values, handleSubmit, handleChange }) => (
+        <section className='form-style-container'>
+          <form onSubmit={handleSubmit}>
+          {/* <form ref={form} onSubmit={sendEmail}> */}
+            <div className='input-style'>
+              <label for="input">Nombre:</label>
+              <input 
+                type="text" 
+                placeholder="Aqui tu nombre" 
+                name="user_name"
+                value={values.name}
+                onChange={handleChange}
+              />
+            </div>  
+
+            <div className='input-style'>
+              <label for="input">Apellido/s:</label>
+              <input type="text" placeholder="Aqui tu apellido/s" name="user_last_name"/>
+            </div>
+
+            <div className='input-style'>
+              <label for="input">Mail</label>
+              <input 
+                type="email" 
+                placeholder="Aqui tu mail" 
+                name="user_email"
+                value={values.email}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className='input-style'>
+              <label for="input">Mensaje</label>
+              <textarea name="message" placeholder="Escribe lo que quieras" />
+            </div>
+
+            <Button variant="outline-primary" type="submit">Enviar</Button>
+          </form>  
+        </section>
+      )}
+    </Formik>
+    
   )
 }
 
