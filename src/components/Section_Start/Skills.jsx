2a189ddf03024ad92e382f55490b109211d220ec
Franco Gallo <gallofrancogabriel@gmail.com>
2023-09-06 11:20:50 +0200

    Adding content to the skills section

--- src/components/Section_Start/Skills.jsx
+++ src/components/Section_Start/Skills.jsx
@@ -8,14 +8,14 @@ const Skills = () => {
   return (
     <section className='skills-container'>
       <div className='skills-btn' onClick={toggle}>
-        <svg className={content && 'svg-rotate'} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <svg className={content && 'left-arrow'} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
         </svg>
 
         <h3>Tecnologías y herramientas</h3>
 
-        <svg className={content && 'svg-rotate'} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+        <svg className={content ? 'right-arrow' : 'svg'} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"/>
         </svg>
       </div>
 
--- src/components/Section_Start/SkillsContent.jsx
+++ src/components/Section_Start/SkillsContent.jsx
@@ -2,29 +2,37 @@ import React from 'react'
 
 const SkillsContent = ( { content } ) => {
 
-  const porcentaje = '60%'
-
-  const width = {
-    width: porcentaje
-  }
+  const skills = [
+    {name: 'HTML', img: 'images/iconHTML.svg', porcentaje: '80%', width: {width:'80%'}},
+    {name: 'CSS', img: 'images/iconCSS.svg', porcentaje: '30%', width: {width:'30%'}},
+    {name: 'SASS', img: 'images/iconSASS.svg', porcentaje: '30%', width: {width:'30%'}},
+    {name: 'Bootstrap', img: 'images/iconBootstrap.svg', porcentaje: '30%', width: {width:'30%'}},
+    {name: 'JavaScript', img: 'images/iconJavaScript.svg', porcentaje: '30%', width: {width:'30%'}},
+    {name: 'React', img: 'images/iconReact.svg', porcentaje: '30%', width: {width:'30%'}},
+    {name: 'Firebase', img: 'images/iconFirebase.svg', porcentaje: '30%', width: {width:'30%'}}
+  ]
 
   return (
     <div className={content ? 'skills-content' : 'skills-content-none'}>
-        <div className='skills'>
-            <img src="images/fotoPrueba.jpg" alt="" />
-
-            <div className='skills-description'>
-                <div className='loading-bar'>
-                    <div className='loading' style={width}></div>
-                </div>
-
-                <div className='skill-name'>
-                    <span className='name'>HTML</span>
-
-                    <span className='percentage'>{porcentaje}</span>
+    {skills.map((skill) => {
+        return (
+            <div className='skills' key={skill.name}>
+                <img src={skill.img} alt={skill.name} />
+    
+                <div className='skills-description'>
+                    <div className='loading-bar'>
+                        <div className='loading' style={skill.width}></div>
+                    </div>
+    
+                    <div className='skill-name'>
+                        <span className='name'>{skill.name}</span>
+    
+                        <span className='percentage'>{skill.porcentaje}</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        )
+    })}
     </div>
   )
 }
